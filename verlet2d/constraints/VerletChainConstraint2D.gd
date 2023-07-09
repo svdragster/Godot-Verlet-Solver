e8b5002e5fe2f6ea1ed04bb951114fb230077195
svdragster <svdragster@gmail.com>
2023-07-09 22:59:17 +0200

    damp velocity for chain

--- verlet2d/constraints/VerletChainConstraint2D.gd
+++ verlet2d/constraints/VerletChainConstraint2D.gd
@@ -7,7 +7,7 @@ var object_b : Node2D
 var length : float
 var _length_squared : float
 
-var max_difference : float = 10.0
+var max_difference : float = 0.1
 
 func _init(object_a : Node2D, object_b : Node2D, length : float):
 	self.object_a = object_a
@@ -16,11 +16,15 @@ func _init(object_a : Node2D, object_b : Node2D, length : float):
 	_length_squared = length * length
 	
 func update_constraint(delta : float) -> void:
-	var v = object_b.position - object_a.position
-	var actual_length_squared = v.length_squared()
+	var v : Vector2 = object_b.position - object_a.position
+	var actual_length_squared : float = v.length_squared()
 	#print(sqrt(actual_length_squared), " ", sqrt(actual_length_squared) - sqrt(_length_squared))
-	if actual_length_squared - _length_squared > max_difference:
+	var distance : float = actual_length_squared - _length_squared
+	if distance > max_difference:
+		var damping := 100.0
+		if distance < 5.0*5.0:
+			damping = 200.0
 		if not object_a.is_static:
-			object_a.position += v/200.0
+			object_a.position += v/damping
 		if not object_b.is_static:
-			object_b.position += v/-200.0
+			object_b.position += v/-damping
